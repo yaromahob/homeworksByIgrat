4f93bd2de30cf36fc3d5246e65459df4ac957fdf
Alexandr <yaromahob@gmail.com>
2022-11-13 20:09:20 +0300

    feat: HW9 from Ignat completed

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -4,55 +4,55 @@ import React, {
   InputHTMLAttributes,
   KeyboardEvent,
   ReactNode,
-} from 'react'
-import s from './SuperInputText.module.css'
+} from 'react';
+import s from './SuperInputText.module.css';
 
 // тип пропсов обычного инпута
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement>
+type DefaultInputPropsType = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута, кроме type
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
 type SuperInputTextPropsType = Omit<DefaultInputPropsType, 'type'> & {
   // и + ещё пропсы которых нет в стандартном инпуте
-  onChangeText?: (value: string) => void
-  onEnter?: () => void
-  error?: ReactNode
-  spanClassName?: string
-}
+  onChangeText?: (value: string) => void;
+  onEnter?: () => void;
+  error?: ReactNode;
+  spanClassName?: string;
+};
 
-const SuperInputText: React.FC<SuperInputTextPropsType> = (
-  {
-    onChange,
-    onChangeText,
-    onKeyPress,
-    onEnter,
-    error,
-    className,
-    spanClassName,
-    id,
-    
-    ...restProps // все остальные пропсы попадут в объект restProps
-  }
-) => {
+const SuperInputText: React.FC<SuperInputTextPropsType> = ({
+  onChange,
+  onChangeText,
+  onKeyPress,
+  onEnter,
+  error,
+  className,
+  spanClassName,
+  id,
+
+  ...restProps // все остальные пропсы попадут в объект restProps
+}) => {
   const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-    onChange?.(e) // если есть пропс onChange, то передать ему е (поскольку onChange не обязателен)
-    
-    onChangeText?.(e.currentTarget.value)
-  }
+    onChange?.(e); // если есть пропс onChange, то передать ему е (поскольку onChange не обязателен)
+
+    onChangeText?.(e.currentTarget.value);
+  };
   const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
-    onKeyPress?.(e)
-    
+    onKeyPress?.(e);
+
     onEnter && // если есть пропс onEnter
-    e.key === 'Enter' && // и если нажата кнопка Enter
-    onEnter() // то вызвать его
-  }
-  const finalSpanClassName = s.error
-    + (spanClassName ? ' ' + spanClassName : '')
-  const finalInputClassName = s.input
-    + (error ? ' ' + s.errorInput : ' ' + s.superInput)
-    + (className ? ' ' + className : '') // задача на смешивание классов
-  
+      e.key === 'Enter' && // и если нажата кнопка Enter
+      onEnter(); // то вызвать его
+  };
+  const finalSpanClassName = s.error + (spanClassName ? ' ' + spanClassName : '');
+  const finalInputClassName =
+    s.input +
+    (error ? ' ' + s.errorInput : ' ' + s.superInput) +
+    (className ? ' ' + className : ''); // задача на смешивание классов
+
   return (
     <div className={s.inputWrapper}>
       <input
@@ -63,14 +63,11 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
         className={finalInputClassName}
         {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
       />
-      <span
-        id={id ? id + '-span' : undefined}
-        className={finalSpanClassName}
-      >
-                {error}
-            </span>
+      <span id={id ? id + '-span' : undefined} className={finalSpanClassName}>
+        {error}
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default SuperInputText
+export default SuperInputText;
--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,86 +1,98 @@
-import React, {useState} from 'react'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import {restoreState} from '../hw06/localStorage/localStorage'
-import s from './Clock.module.css'
+import React, {useState} from 'react';
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton';
+import {restoreState} from '../hw06/localStorage/localStorage';
+import s from './Clock.module.css';
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 function Clock() {
-    const [timerId, setTimerId] = useState<number | undefined>(undefined)
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
-    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
-    const [show, setShow] = useState<boolean>(false)
-
-    const start = () => {
-        // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
-        // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
-    }
-
-    const stop = () => {
-        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
-    }
-
-    const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
-    }
-    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
-    }
-
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
-
-    return (
-        <div className={s.clock}>
-            <div
-                id={'hw9-watch'}
-                className={s.watch}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                <span id={'hw9-day'}>{stringDay}</span>,{' '}
-                <span id={'hw9-time'}>
+  const [timerId, setTimerId] = useState<number | undefined>(undefined);
+  // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
+  const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())));
+  const [show, setShow] = useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+  const start = () => {
+    stop();
+    // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
+    // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
+    const idTimer: number = window.setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    setTimerId(idTimer);
+    setIsDisabled(!isDisabled);
+  };
+  
+  
+  const stop = () => {
+    clearInterval(timerId);
+    // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
+    setIsDisabled(!isDisabled);
+  };
+  
+  const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
+    setShow(!show);
+  };
+  const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
+    setShow(!show);
+  };
+  const secondsForTime = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+  const minutesForTime = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+  const hoursForTime = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
+  const stringTime = `${hoursForTime}:${minutesForTime}:${secondsForTime}`; // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+  const stringDate = date.toLocaleDateString(); // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+  
+  // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
+  const stringDay = days[date.getDay()]; // пишут студенты
+  const stringMonth = months[date.getMonth()]; // пишут студенты
+  
+  return (
+    <div className={s.clock}>
+      <div
+        id={'hw9-watch'}
+        className={s.watch}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        <span id={'hw9-day'}>{stringDay}</span>,{' '}
+        <span id={'hw9-time'}>
                     <strong>{stringTime}</strong>
                 </span>
-            </div>
-
-            <div id={'hw9-more'}>
-                <div className={s.more}>
-                    {show ? (
-                        <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDate}</span>
-                        </>
-                    ) : (
-                        <>
-                            <br/>
-                        </>
-                    )}
-                </div>
-            </div>
-
-            <div className={s.buttonsContainer}>
-                <SuperButton
-                    id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
-                    onClick={start}
-                >
-                    start
-                </SuperButton>
-                <SuperButton
-                    id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
-                    onClick={stop}
-                >
-                    stop
-                </SuperButton>
-            </div>
+      </div>
+      
+      <div id={'hw9-more'}>
+        <div className={s.more}>
+          {show ? (
+            <>
+              <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+              <span id={'hw9-date'}>{stringDate}</span>
+            </>
+          ) : (
+            <>
+              <br/>
+            </>
+          )}
         </div>
-    )
+      </div>
+      
+      <div className={s.buttonsContainer}>
+        <SuperButton
+          id={'hw9-button-start'}
+          disabled={isDisabled} // пишут студенты // задизэйблить если таймер запущен
+          onClick={start}
+        >
+          start
+        </SuperButton>
+        <SuperButton
+          id={'hw9-button-stop'}
+          disabled={!isDisabled} // пишут студенты // задизэйблить если таймер не запущен
+          onClick={stop}
+        >
+          stop
+        </SuperButton>
+      </div>
+    </div>
+  );
 }
 
-export default Clock
+export default Clock;
