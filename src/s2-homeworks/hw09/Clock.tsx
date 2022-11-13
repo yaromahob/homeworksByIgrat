c273fcc2d19b4f8397738e639eb2e4571b477329
Alexandr <yaromahob@gmail.com>
2022-11-13 20:18:43 +0300

    fix: fix stringDate value

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -40,7 +40,7 @@ function Clock() {
   const minutesForTime = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
   const hoursForTime = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
   const stringTime = `${hoursForTime}:${minutesForTime}:${secondsForTime}`; // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-  const stringDate = date.toLocaleDateString(); // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+  const stringDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
   
   // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
   const stringDay = days[date.getDay()]; // пишут студенты
