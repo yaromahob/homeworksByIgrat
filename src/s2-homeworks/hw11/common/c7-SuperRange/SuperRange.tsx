a8eb9d8c67a73ff13cbe542f7f7524cf335704a4
Alexandr <yaromahob@gmail.com>
2022-11-22 22:56:34 +0300

    feat: hw11 from ignat completed

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import React from 'react';
+import {Slider, SliderProps} from '@mui/material';
+
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
-}
+  return (
+    <Slider
+      sx={{ // стили для слайдера // пишет студент
+        color: '#00CC22',
+        '& .MuiSlider-rail': {
+          color: "#8B8B8B",
+        },
+        
+        // '& #hw11-single-slider:first-child': {
+        //   width: '185px'
+        // },
+        '& .MuiSlider-thumb': {
+          color: '#fff',
+          
+          border: '2px solid #00CC22',
+        },
+        '& .MuiSlider-thumb:after': {
+          width: '6px',
+          height: '6px',
+          top: '50%',
+          left: '50%',
+          backgroundColor: '#00CC22',
+        }
+        
+      }}
+      getAriaLabel={() => 'Temperature range'}
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  );
+};
 
-export default SuperRange
+export default SuperRange;
