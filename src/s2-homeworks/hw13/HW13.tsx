f78751d093099ab6ae1264f65732ecd76c3e578e
Alexandr <yaromahob@gmail.com>
2022-12-03 13:46:20 +0300

    fix: text about error added

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,7 +36,8 @@ const HW13 = () => {
       .then((res) => {
         setCode('Код 200!');
         setImage(success200);
-        setInfo('...всё ок) код 200 - обычно означает что скорее всего всё ок)');
+        setText('код 200 - обычно означает что скорее всего всё ок)');
+        setInfo('...всё ок)');
         // дописать
         
       })
@@ -44,19 +45,21 @@ const HW13 = () => {
         if (e.response.status === 400) {
           setCode('Ошибка 400!');
           setImage(error400);
-          setInfo('Ты не отправил success в body вообще! ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!');
+          setText('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!');
+          setInfo('Ты не отправил success в body вообще!');
         }
         if (e.response.status === 500) {
           setCode('Ошибка 500!');
           setImage(error500);
+          setText('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных');
           setInfo(
-            'эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных');
+            'эмитация ошибки на сервере');
         }
         if (e.response.status === 0) {
           setCode('Error!');
           setImage(errorUnknown);
-          setInfo('Network Error\n' +
-            'AxiosError');
+          setText('AxiosError');
+          setInfo('Network Error');
         }
         // дописать
       })
