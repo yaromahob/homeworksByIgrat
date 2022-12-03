3ed0fcf43c7d26464cef134420a5f1843af40c59
Alexandr <yaromahob@gmail.com>
2022-12-03 13:51:46 +0300

    fix: ыswapped setInfo with setText

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,8 +36,8 @@ const HW13 = () => {
       .then((res) => {
         setCode('Код 200!');
         setImage(success200);
-        setText('код 200 - обычно означает что скорее всего всё ок)');
-        setInfo('...всё ок)');
+        setInfo('код 200 - обычно означает что скорее всего всё ок)');
+        setText('...всё ок)');
         // дописать
         
       })
@@ -45,21 +45,21 @@ const HW13 = () => {
         if (e.response.status === 400) {
           setCode('Ошибка 400!');
           setImage(error400);
-          setText('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!');
-          setInfo('Ты не отправил success в body вообще!');
+          setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!');
+          setText('Ты не отправил success в body вообще!');
         }
         if (e.response.status === 500) {
           setCode('Ошибка 500!');
           setImage(error500);
-          setText('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных');
-          setInfo(
+          setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных');
+          setText(
             'эмитация ошибки на сервере');
         }
         if (e.response.status === 0) {
           setCode('Error!');
           setImage(errorUnknown);
-          setText('AxiosError');
-          setInfo('Network Error');
+          setInfo('AxiosError');
+          setText('Network Error');
         }
         // дописать
       })
